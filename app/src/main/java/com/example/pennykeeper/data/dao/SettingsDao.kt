451c5a0966fbee6561df38b8d2c90042e27424f3
mj326 <minjun32622@gmail.com>
2024-11-17 02:40:43 +0800

    modified Budget Setting Function

--- app/src/main/java/com/example/pennykeeper/data/dao/SettingsDao.kt
+++ app/src/main/java/com/example/pennykeeper/data/dao/SettingsDao.kt
@@ -2,6 +2,7 @@ package com.example.pennykeeper.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.pennykeeper.data.model.Budget
 import kotlinx.coroutines.flow.Flow
@@ -9,9 +10,11 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface SettingsDao {
 
-    @Insert
-    suspend fun insert(budget: Budget)
 
     @Query("SELECT * FROM settings_table ORDER BY id DESC LIMIT 1")
-    fun getCurrentBudget(): Flow<Budget>
+    fun getBudget(): Flow<Budget?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(budget: Budget)
+
 }
--- app/src/main/java/com/example/pennykeeper/data/repository/SettingsRepository.kt
+++ app/src/main/java/com/example/pennykeeper/data/repository/SettingsRepository.kt
@@ -3,12 +3,16 @@ package com.example.pennykeeper.data.repository
 import com.example.pennykeeper.data.dao.SettingsDao
 import com.example.pennykeeper.data.model.Budget
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class SettingsRepository(private val settingsDao: SettingsDao) {
 
     // Get current budget from the database
     fun getCurrentBudget(): Flow<Budget> {
-        return settingsDao.getCurrentBudget()
+        return settingsDao.getBudget()
+            .map { budget ->
+                budget ?: Budget(dailyBudget = 0.0) // Provide default Budget if null
+            }
     }
 
     // Save new budget to the database
